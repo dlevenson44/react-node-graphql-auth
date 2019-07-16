10ea9ec71b0b270202d97ecc2a9279dddeffafa9
dlevenson44 <daniellevenson4@gmail.com>
2019-07-16 18:40:33 -0400

    start setting up rootquery

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,8 +1,12 @@
 const graphql = require('graphql');
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLID } = graphql;
 
+// one field must be assigned per type to avoid GraphQL errors
 const RootQueryType = new GraphQLObjectType({
-  name: 'RootQueryType'
+  name: 'RootQueryType',
+  fields: {
+    dummyField: { type: GraphQLID }
+  }
 });
 
 module.exports = RootQueryType;
